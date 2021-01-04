2aad297a207d4462e3fc263c17d354726d8ead2e
TCFKARADENIZ <faruk.karadeniz@turkcell.com.tr>
2021-01-04 18:12:03 +0300

    finish

--- src/main/components/BookComponent.js
+++ src/main/components/BookComponent.js
@@ -11,10 +11,10 @@ class BookComponent extends Component {
         <div className="books-grid">
             {
                 this.props.books.map(book => (
-                    <li key={book.id}>
+                    <li key={Math.random().toString(36)}>
                         <div className="book">
                             <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : ''})` }}></div>
                                 <ChangeShelfComponent shelftTypes={this.props.shelfList} shelfValue={book.shelfValue ? book.shelfValue : 'none'} changeShelftValue={this.props.changeShelftValue} bookId={book.id ? book.id : ''}/>
                             </div>
                             <div className="book-title">{book.title}</div>
